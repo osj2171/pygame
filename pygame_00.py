da8929c877704d08aa6495f7d8fdac3cb2310627
samtae13 <ekfrlsms22@gmail.com>
2020-04-11 14:18:15 +0900

    저장소 만들기

---
+++ pygame_00.py
@@ -0,0 +1,14 @@
+'''
+
+    2020.04.11
+    pygame 저장소 만들기
+
+    1. 'pygame' Project 생성하기
+    2. pygame module 설치하기
+        2-1. file / setting / pygame interpreter
+    3. git과 연동하기
+        3-1. vcs/ import into version control / share project on github
+        3-2. venv 가상환경 연동 헤제 하기
+    4. pygame 00.py 파일 commit
+    5. pygame 00.py 파일 git repository / push
+    6. github site 확인
